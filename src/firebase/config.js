d86872a978321009b4f8179e90ec2fc7237e79f3
ProudBloom <jakub-sztompka@wp.pl>
2023-02-04 18:14:34 +0100

    Firebase setup (firestore + auth)

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+   apiKey: 'AIzaSyDSvxT2xQy_4TSFbwuj4EQmMjy71SjKWSs',
+   authDomain: 'the-dojo-cbd3a.firebaseapp.com',
+   projectId: 'the-dojo-cbd3a',
+   storageBucket: 'the-dojo-cbd3a.appspot.com',
+   messagingSenderId: '803828514618',
+   appId: '1:803828514618:web:d8bbc4e7537aa554615a03',
+};
+
+firebase.initializeApp();
+
+const firestoreDatabase = firebase.firestore();
+const projectAuth = firebase.auth();
+
+const timestamp = firebase.firestore.Timestamp;
+
+export { firestoreDatabase, projectAuth, timestamp };
