34332818e1e1a451dbfa76391248307235ceafed
ProudBloom <jakub-sztompka@wp.pl>
2023-03-08 23:57:12 +0100

    Project page component

--- src/pages/projectPage/ProjectPage.js
+++ src/pages/projectPage/ProjectPage.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useDocument } from '../../hooks/useDocument';
 
 //styles
 import './ProjectPage.css';
 
 export default function ProjectPage() {
-   return <div>ProjectPage</div>;
+   const { projectID } = useParams();
+   const { document: project, error } = useDocument('projects', projectID);
+
+   if (error) {
+      return (
+         <div className='flex-center flex-column'>
+            <h3>{error}</h3>
+            <Link to='/' className='btn mt-1'>
+               Back to Dashboard
+            </Link>
+         </div>
+      );
+   }
+
+   if (!project) {
+      return <div className='loading'>Loading...</div>;
+   }
+
+   return (
+      <div className='project-details'>
+         <h1>{project.name}</h1>
+      </div>
+   );
 }
